c7562c6d42979fba516c6731a0d4f685906ac63b
Oke Schwien <okeschwien@gmail.com>
2016-12-30 00:25:52 +0100

    Fixed 00:00:00 bug

--- js/countdown.js
+++ js/countdown.js
@@ -5,17 +5,17 @@ function startTimer() {
     var minutesLeft = 59 - today.getMinutes();
     var secondsLeft = 59 - today.getSeconds();
 
-    if (Math.floor(secondsLeft) > 0) {
+    if (Math.floor(secondsLeft) <= 0 && Math.floor(minutesLeft) <= 0 && Math.floor(hoursLeft) <= 0) {
+        document.getElementById('countdown-chars').innerHTML = "00:00:00";
+    }
+    else {
         document.getElementById('countdown-chars').innerHTML =
         wrapChars(hoursLeft, -2) + ":" +
         wrapChars(minutesLeft, -2) + ":" +
         wrapChars(secondsLeft, -2);
     }
-    else {
-        document.getElementById('countdown-chars').innerHTML = "00:00:00";
-    }
 
-    setTimeout('startTimer()', 1000);
+    setTimeout('startTimer()', 999);
 }
 
 function wrapChars(string, maxDigits) {
