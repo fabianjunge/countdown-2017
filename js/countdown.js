3dd393b321cd49b3f65831de8673cdbb14596193
Oke Schwien <okeschwien@gmail.com>
2016-12-30 00:14:42 +0100

    Added countdown code

---
+++ js/countdown.js
@@ -0,0 +1,26 @@
+function startTimer() {
+    var today = new Date();
+
+    var hoursLeft = 23 - today.getHours();
+    var minutesLeft = 59 - today.getMinutes();
+    var secondsLeft = 59 - today.getSeconds();
+
+    if (Math.floor(secondsLeft) > 0) {
+        document.getElementById('countdown-chars').innerHTML =
+        wrapChars(hoursLeft, -2) + ":" +
+        wrapChars(minutesLeft, -2) + ":" +
+        wrapChars(secondsLeft, -2);
+    }
+    else {
+        document.getElementById('countdown-chars').innerHTML = "00:00:00";
+    }
+
+    setTimeout('startTimer()', 1000);
+}
+
+function wrapChars(string, maxDigits) {
+    string = (Math.pow(10, Math.abs(maxDigits)) + string.toString()).slice(maxDigits);
+    return string;
+}
+
+startTimer();
